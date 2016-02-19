e841ea85698fa40072f73aef5b3b00b350c7978b
Marc A. Anderson <marc@marcadam.com>
2016-02-18 22:28:52 -0800

    Create file to hold constants

---
+++ Twitter/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Twitter
+//
+//  Created by Marc Anderson on 2/18/16.
+//  Copyright © 2016 Marc Anderson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Color {
+    static let twitterBlue = UIColor(red: 85.0/255.0, green: 172.0/255.0, blue: 238.0/255.0, alpha: 1.0)
+}
